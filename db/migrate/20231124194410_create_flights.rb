06956c1306f08f143bb10379c93e4a9c160cdfea
blainefreestone <blainefreestone01@gmail.com>
2023-11-24 12:05:53 -0800

    Schema supports departing and arriving flights foreign keys in both the Flight and Airport model.

--- db/migrate/20231124194410_create_flights.rb
+++ db/migrate/20231124194410_create_flights.rb
@@ -1,10 +1,10 @@
 class CreateFlights < ActiveRecord::Migration[7.1]
   def change
     create_table :flights do |t|
-      t.references :departure_airport, null: false, foreign_key: true
-      t.references :arrival_airport, null: false, foreign_key: true
+      t.references :departure_airport, null: false, foreign_key: { to_table: :airports }
+      t.references :arrival_airport, null: false, foreign_key: { to_table: :airports }
       t.datetime :start_datetime
-      t.integer :flight_duration_minutes
+      t.integer :duration_minutes
 
       t.timestamps
     end
