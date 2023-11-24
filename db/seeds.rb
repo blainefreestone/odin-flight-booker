020bc0a0fbc30d9eecf53cc4ceafcf3bfb00018f
blainefreestone <blainefreestone01@gmail.com>
2023-11-24 11:42:28 -0800

    Airport model created with scaffolding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Seed 30 airports in the United States.
+Airport.delete_all
+
+airport_codes = ["ATL","DFW","DEN","ORD","LAX","JFK","LAS","MCO","MIA","CLT","SEA","PHX","EWR","SFO","IAH","BOS","FLL","MSP","LGA","DTW","PHL","SLC","DCA","SAN","BWI","TPA","AUS","IAD","BNA","MDW"]
+
+airport_codes.each do |code|
+    Airport.create!(code: code)
+end
