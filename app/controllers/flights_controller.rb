c71457c12b8d26d851c4b4be18a4b29796831172
blainefreestone <blainefreestone01@gmail.com>
2023-11-29 09:16:39 -0800

    Updated flights index to show flights only after search.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,17 +1,19 @@
 class FlightsController < ApplicationController
   include Pagy::Backend
 
-  before_action :set_flight, only: %i[ show edit update destroy ]
-
   # GET /flights or /flights.json
   def index
     @num_passengers_options = (1..4).map { |num| [num, num] }.insert(0, nil)
     @airport_options = Airport.all.map { |airport| [ airport.code, airport.id ] }.insert(0, nil)
 
-    @pagy, @flights = pagy(Flight.around_date(params[:date])
-                                 .from_departure_airport(params[:departure_airport_id])
-                                 .to_arrival_airport(params[:arrival_airport_id])
-                                 .order(:start_datetime))
+    if params[:date] || params[:departure_airport_id] || params[:arrival_airport_id]
+      @pagy, @flights = pagy(Flight.around_date(params[:date])
+                                  .from_departure_airport(params[:departure_airport_id])
+                                  .to_arrival_airport(params[:arrival_airport_id])
+                                  .order(:start_datetime))
+    else
+      @flights = nil
+    end
 
     if params[:num_passengers].nil? || params[:num_passengers] == ""
       @num_passengers = 1
