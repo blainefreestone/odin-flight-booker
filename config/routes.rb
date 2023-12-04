1066abfd94eb548025d082598997d4d1ff5bb75d
blainefreestone <blainefreestone01@gmail.com>
2023-12-03 23:21:33 -0800

    Generated User and Admin models with helper files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
   resources :flights, only: [:index]
   resources :bookings, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
