4fde33c71a48d4a149b0be207b6f9459e2f9317a
blainefreestone <blainefreestone01@gmail.com>
2023-12-05 23:36:25 -0800

    Airport and passenger specs written.

--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
+require 'cancan/matchers'
 
-# RSpec.describe Airport, type: :model do
-#   it "factory sequence works" do
-#     airport1 = create(:airport)
-#     airport2 = create(:airport)
-#     airport3 = create(:airport)
-#     expect(airport1.code).to eql "AAA"
-#     expect(airport2.code).to eql "AAB"
-#     expect(airport3.code).to eql "AAC"
-#   end
-# end
+RSpec.describe Airport, type: :model do
+  describe "User" do
+    describe "abilities" do
+      subject(:ability) { Ability.new(user) }
+      let(:user) { nil }
+
+      describe "when is admin" do
+        let(:user) { create(:user, admin: true) }
+
+        it { is_expected.to be_able_to(:manage, build(:airport)) }
+      end
+
+      describe "when is not admin" do
+        let(:user) { create(:user, admin: false) }
+
+        it { is_expected.not_to be_able_to(:read, build(:airport)) }
+        it { is_expected.not_to be_able_to(:create, build(:airport)) }
+        it { is_expected.not_to be_able_to(:update, build(:airport)) }
+        it { is_expected.not_to be_able_to(:destroy, build(:airport)) }
+      end
+    end
+  end
+end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
+require 'cancan/matchers'
 
-# RSpec.describe Passenger, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Passenger, type: :model do
+  describe "User" do
+    describe "abilities" do
+      subject(:ability) { Ability.new(user) }
+      let(:user) { nil }
+
+      describe "when is admin" do
+        let(:user) { create(:user, admin: true) }
+
+        it { is_expected.to be_able_to(:manage, build(:passenger)) }
+      end
+
+      describe "when is not admin" do
+        let(:user) { create(:user, admin: false) }
+
+        it { is_expected.not_to be_able_to(:read, build(:passenger)) }
+        it { is_expected.not_to be_able_to(:create, build(:passenger)) }
+        it { is_expected.not_to be_able_to(:update, build(:passenger)) }
+        it { is_expected.not_to be_able_to(:destroy, build(:passenger)) }
+      end
+    end
+  end
+end
